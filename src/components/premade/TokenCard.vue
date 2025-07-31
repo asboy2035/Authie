c2cf4a46f5ba0afbec1234611b2ecaf7005d01ba
ash <ash@asboy2035.com>
2025-07-31 21:22:54 +0200

    add token generation

--- src/components/premade/TokenCard.vue
+++ src/components/premade/TokenCard.vue
@@ -1,52 +1,110 @@
 <script setup lang="ts">
+  import { computed, onMounted, onUnmounted, ref } from 'vue'
+  import * as OTPAuth from 'otpauth'
   import HStack from '@/components/layout/HStack.vue'
   import VStack from '@/components/layout/VStack.vue'
   import InteriorItem from '@/components/layout/InteriorItem.vue'
+  import {Icon} from '@iconify/vue'
 
-  defineProps<{
+  const props = defineProps<{
     name: string
     username: string
-    token: string
+    secret: string
     color: string
+    timeRemaining: number
   }>()
+
+  const emit = defineEmits(['edit', 'delete'])
+
+  const totp = new OTPAuth.TOTP({
+    issuer: 'Authie',
+    label: props.name,
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: props.secret
+  })
+
+  const token = ref(totp.generate())
+  let interval: number
+
+  const isExpiring = computed(() => props.timeRemaining <= 5)
+
+  onMounted(() => {
+    interval = setInterval(() => {
+      token.value = totp.generate()
+    }, 1000)
+  })
+
+  onUnmounted(() => {
+    clearInterval(interval)
+  })
 </script>
 
 <template>
-  <InteriorItem class="token">
-    <HStack class="tokenContent">
-      <div
-        class="tokenColor"
-        :style="{ '--color': color }"
-      />
+  <InteriorItem class="token" :class="{ expiring: isExpiring }">
+    <HStack class="autoSpace fullWidth">
+      <HStack class="tokenContent">
+        <div
+          class="tokenColor"
+          :style="{ '--color': color }"
+        />
 
-      <VStack class="tokenInfo">
-        <VStack class="tokenName">
-          <h4>{{ name }}</h4>
-          <p class="light">{{ username }}</p>
+        <VStack class="tokenInfo">
+          <VStack class="tokenName">
+            <h3>{{ name }}</h3>
+            <p class="light">{{ username }}</p>
+          </VStack>
+
+          <p class="tokenText">{{ token }}</p>
         </VStack>
+      </HStack>
 
-        <p class="tokenText">{{ token }}</p>
-      </VStack>
+      <!--Actions-->
+      <HStack class="tokenActions">
+        <button class="transparent" @click="emit('edit')">
+          <Icon icon="solar:pen-2-line-duotone" />
+        </button>
+        <button class="transparent" @click="emit('delete')">
+          <Icon icon="solar:trash-bin-minimalistic-line-duotone" />
+        </button>
+      </HStack>
     </HStack>
   </InteriorItem>
 </template>
 
 <style scoped lang="sass">
+  @use "@/styles/animations"
+
   .token
     align-items: flex-start
 
-    .tokenContent
-      .tokenColor
-        height: calc(100% - 2rem)
-        width: 0.25rem
-        border-radius: 0.25rem
-        background: var(--color)
-        opacity: 0.4
+    &.expiring
+      animation: pulse 0.8s ease infinite
 
-      .tokenInfo
-        .tokenName
-          gap: 0
+  .tokenContent
+    .tokenColor
+      width: 0.25rem
+      margin: 0.5rem 0
+      height: auto
+      min-height: 3rem
+      border-radius: 0.25rem
+      background: var(--color)
+      opacity: 0.4
 
-        .tokenText
-          font-size: 200%
+    .tokenInfo
+      .tokenName
+        gap: 0.1rem
+
+        *
+          margin: 0
+
+      .tokenText
+        font-size: 200%
+
+  .tokenActions
+    button
+      svg
+        width: 1.5rem !important
+        height: 1.5rem !important
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import { onMounted, onUnmounted, ref } from 'vue'
   import Card from '@/components/layout/Card.vue'
   import CardTitle from '@/components/layout/CardTitle.vue'
   import NavBar from '@/components/premade/navbar/NavBar.vue'
@@ -6,19 +7,35 @@
   import NavigationTitle from '@/components/navigation/NavigationTitle.vue'
   import {Icon} from '@iconify/vue'
   import Modal from '@/components/layout/Modal.vue'
-  import {ref} from 'vue'
   import Grid from '@/components/layout/Grid.vue'
   import TokenCard from '@/components/premade/TokenCard.vue'
   import { useTokensStore } from '@/stores/tokens'
+  import InteriorItem from '@/components/layout/InteriorItem.vue'
 
   const tokensStore = useTokensStore()
 
   const showingCreateModal = ref<boolean>(false)
+  const showingEditModal = ref<boolean>(false)
+  const editingToken = ref<number | null>(null)
 
   const newName = ref('')
   const newUsername = ref('')
   const newColor = ref('#ff0000')
   const newSecret = ref('')
+
+  const timeRemaining = ref(0)
+  let interval: number
+
+  onMounted(() => {
+    interval = setInterval(() => {
+      const date = new Date()
+      timeRemaining.value = 30 - (date.getSeconds() % 30)
+    }, 1000)
+  })
+
+  onUnmounted(() => {
+    clearInterval(interval)
+  })
 
   const createToken = () => {
     tokensStore.addToken({
@@ -29,11 +46,43 @@
     })
     showingCreateModal.value = false
   }
+
+  const editToken = (index: number) => {
+    const token = tokensStore.tokens[index]
+    if (token) {
+      newName.value = token.name
+      newUsername.value = token.username
+      newColor.value = token.color
+      newSecret.value = token.secret
+      editingToken.value = index
+      showingEditModal.value = true
+    }
+  }
+
+  const updateToken = () => {
+    if (editingToken.value !== null) {
+      tokensStore.tokens[editingToken.value] = {
+        name: newName.value,
+        username: newUsername.value,
+        color: newColor.value,
+        secret: newSecret.value
+      }
+      showingEditModal.value = false
+      editingToken.value = null
+    }
+  }
+
+  const deleteToken = (index: number) => {
+    tokensStore.tokens.splice(index, 1)
+  }
 </script>
 
 <template>
   <ContentView id="homeView">
     <NavigationTitle title="Authie">
+      <InteriorItem id="otpTime">
+        <p>{{ timeRemaining }}</p>
+      </InteriorItem>
       <button
         @click="showingCreateModal = !showingCreateModal"
       >
@@ -73,8 +122,11 @@
           :key="index"
           :name="token.name"
           :username="token.username"
-          token="678455"
+          :secret="token.secret"
           :color="token.color"
+          :time-remaining="timeRemaining"
+          @edit="editToken(index)"
+          @delete="deleteToken(index)"
         />
       </Grid>
     </Card>
@@ -97,9 +149,25 @@
       Create
     </button>
   </Modal>
+
+  <Modal v-if="showingEditModal">
+    <CardTitle
+      title="Edit Token"
+      icon="solar:pen-2-line-duotone"
+    />
+
+    <input v-model="newSecret" type="password" placeholder="Secret" />
+    <input v-model="newName" type="text" placeholder="Service" />
+    <input v-model="newUsername" type="text" placeholder="@Username" />
+    <input v-model="newColor" type="color" placeholder="Color" />
+    <button @click="updateToken">
+      <Icon icon="solar:pen-2-line-duotone" />
+      Update
+    </button>
+  </Modal>
 </template>
 
 <style scoped lang="sass">
-  #tokenGrid
-    grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr))
+  #otpTime
+    padding: 0.25rem 0.5rem !important
 </style>
